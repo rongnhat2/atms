54456f752d9088af18894d43cdffb0a8fca2d0fc
Nguyen Nhat Long <long.nguyen2198@gmail.com>
2023-11-15 21:36:00 +0700

    update data

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -14,7 +14,7 @@
             </div>
             <div class="wrapper">
                 <div class="about-wrapper">
-                    <h3 class="about-title">About</h3>
+                    <h3 class="about-title">About Us</h3>
                     <p class="about-description">
                         Established in 2010, AMBO Technologies sells electronic equipment and electronic components. We also provide OEM and ODM services for Viet Nam as long as global companies. We have strong knowledge about designing industrial automation, automotive, telecommunication, and power electronics products.</p>
                     <p class="about-description">We have strong partnerships with various electronics component distributors and mechanical engineering service companies. Our mission is to help our clients to design and manufacture the best electronic products. ATMS was spun off from AMBO's R&D department in 2023. </p>
